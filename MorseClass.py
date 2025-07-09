c6e45e4c9337597e84334fe957305e46ad300307
Nicholas <nicholasbence12@gmail.com>
2025-07-09 11:04:35 +0200

    Version 3. More accurate now.. uses same playsound, but whitespaces and slashes' characterstic have been swopped. It was because I experienced an issue on my laptop which yielded incorrect results.

--- MorseClass.py
+++ MorseClass.py
@@ -53,42 +53,48 @@ class MorseCodeConverter:
     '+': '.-.-.',
     '=': '-...-',
     '-': '-....-',
-    ' ' : '    '
+    ' ' : '/'
 }
     
+    
+
+
+
     def __play_dot(self):
-        playsound('dot.wav')
+        playsound("dot.wav")
 
     def __play_dash(self):
-        playsound('dash.wav')
+        playsound("dash.wav")
 
 # TODO - Loop through user text and transform it to morse code
-    def convert_to_morse(self,text):
+    def convert_to_morse(self,text): # SOS -> ... --- ...
         morse_code = []
         for char in text:
             value = self.MORSE_CODE_DICT.get(char)
             if value is not None:
-                morse_code.append(f"{value}/")
+                morse_code.append(f"{value} ")
             else:
                 print("Invalid character - does not exist within library.")
         morse_word = ''.join(morse_code)
         for c in morse_word:
             if c == '.':
                 self.__play_dot()
-                time.sleep(0.1)
+                time.sleep(0.3)
             elif c== "-":
                 self.__play_dash()
-                time.sleep(0.1)
+                time.sleep(0.3)
             elif c==" ":
+                time.sleep(0.7)
+            elif c=="/":
                 time.sleep(1)
-            else:
-                time.sleep(0.5)
         return morse_word
 
     def convert_to_text(self,morse_code):
         # morse = [chunk for chunk in morse_code.split('/') if len(chunk) !=2 and not chunk.isspace()]
         # morse = re.split(r'( {4})', morse_code)
-        morse = [chunk for chunk in re.split(r'( {4})|/', morse_code) if chunk and (chunk == '    ' or not chunk.isspace())]
+        # .... .. = hi
+        morse = re.split(r"[ ]+",morse_code)
+        # print(morse)
         text = []
         for part in morse:
             key = self.__get_key_by_value(part)
@@ -100,3 +106,21 @@ class MorseCodeConverter:
             if v == target:
                 return k
         return None
+    
+    def test(self):
+        text = "hello world"
+        print(f"Original text: {text}")
+        morse = self.convert_to_morse(text)
+        print(f"Morse code: {morse}")
+    
+    def convert(self):
+        morse = ".... .. / ."
+        print(f"Original text: {morse}")
+        text = self.convert_to_text(morse)
+        print(f"Morse code: {text}")
+
+
+if __name__ == "__main__":
+    converter=MorseCodeConverter()
+    converter.test()
+    converter.convert()
